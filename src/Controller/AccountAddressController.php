fe34eefb12f69cdedb1d073b56e5c2c36442ca1f
jo2u3e <jo2u3.con1ac1@outlook.com>
2021-09-11 20:35:24 -0400

    edit adresse

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -7,10 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\AddressType;
 use App\Entity\Address;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class AccountAddressController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
 
 
     #[Route('/compte/adresses', name: 'account_address')]
@@ -20,14 +31,69 @@ class AccountAddressController extends AbstractController
     }
 
     #[Route('/compte/ajouter-une-adresse', name: 'account_address_add')]
-    public function add(): Response
+    public function add(Request $request): Response
     {
         $address = new Address();
         $form = $this->createForm(AddressType::class, $address);
-        return $this->render('account/add_address.html.twig', [
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $address->setUser($this->getUser());
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
             'form' => $form->createView()
         ]);
     }
+
+
+    #[Route('/compte/modifier-une-adresse/{id}', name: 'account_address_edit')]
+    public function edit(Request $request, $id): Response
+    {
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+        if (!$address || $address->getUser() != $this->getUser()){
+            return $this->redirectToRoute('account_address');
+        }
+
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+           
+           
+            $this->entityManager->flush();
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/compte/supprimer-une-adresse/{id}', name: 'account_address_delete')]
+    public function delete( $id): Response
+    {
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+
+        if ($address || $address->getUser() == $this->getUser()){
+            $this->entityManager->remove($address);
+            $this->entityManager->flush();
+
+        }
+
+           
+           
+          
+            return $this->redirectToRoute('account_address');
+        
+
+    }
+
 
 
 }
--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -7,24 +7,78 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class AddressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+      
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('company')
-            ->add('address')
-            ->add('postal')
-            ->add('city')
-            ->add('country')
-            ->add('phone')
-            ->add('submit', SubmitType::class, [
-                'label' => 'Ajouter mon adresse'
+            ->add('name', TextType::class,[
+                'label' => 'Nommer votre adresse',
+                'attr' => [
+                    'placeholder' => 'Nom de votre adresse'
+                ]
+            ])
+            ->add('lastname', TextType::class,[
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Votre nom'
+                ]
+            ])
+            ->add('firstname', TextType::class,[
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Votre prénom'
+                ]
+            ])
+           
+            ->add('company', TextType::class,[
+                'label' => 'Votre société',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => '(facultatif) Votre société'                    
+                ]
+            ])
+            ->add('address', TextType::class,[
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Votre adresse'
+                ]
+            ])
+            ->add('postal', TextType::class,[
+                'label' => 'Code postal',
+                'attr' => [
+                    'placeholder' => 'Votre code postal'
+                ]
+            ])
+            ->add('city', TextType::class,[
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Votre ville'
+                ]
+            ])
+            ->add('country', CountryType::class,[
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'Votre pays'
+                ]
+            ])
+            ->add('phone', TelType::class,[
+                'label' => 'N° de téléphone',
+                'attr' => [
+                    'placeholder' => 'Votre numéro de téléphone'
+                ]
+            ])
+            ->add('submit',SubmitType::class,[
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn btn-info'
+                ]
             ])
         ;
     }
